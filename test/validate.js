2a9a49e6aa74b6e081f87fe73c606a23c0208f15
Glen Selle <glenselle@me.com>
2014-02-12 12:14:21 -0600

    Added more tests to cover more validation functions.

--- test/validate.js
+++ test/validate.js
@@ -4,7 +4,7 @@ var chai     = require('chai')
 
 describe('Validate', function(){
     describe('Required Fields', function(){
-        it('required fields cannot be left blank', function(){
+        it('required fields must have contents or an error is thrown', function(){
             var testObjectOne = {
                 fieldOne: {
                     name: 'fieldOne',
@@ -16,14 +16,104 @@ describe('Validate', function(){
                 }
             };
 
-            // The test should fail since the field is required but the value is '' (empty string)
+            // The functio should throw an error since the field is required but the value is '' (empty string)
             expect(function () { validate.validateRequiredFields(testObjectOne) }).to.throw('fieldOne is a required field but its value is empty.');
 
             // Change the value to a valid alphanumeric string
             testObjectOne.fieldOne.value = 'some value';
 
-            // Test to make sure it now passes validation
-            expect(function () { validate.validateRequiredFields(testObjectOne) }).to.not.throw(Error);
+            // Make sure the function doesn't throw an error.
+            expect(function () { validate.validateRequiredFields(testObjectOne) }).not.to.throw('fieldOne is a required field but its value is empty.');
+        });
+    });
+
+    describe('Optional Fields', function(){
+        it('optional fields can be left blank without throwing an error', function(){
+            var testObjectOne = {
+                fieldOne: {
+                    name: 'fieldOne',
+                    width: 6,
+                    position: 9,
+                    required: false,
+                    type: 'alphanumeric',
+                    value: ''
+                }
+            };
+
+            // The expect should not throw an error since the field is not required so an empty string is okay.
+            expect(function () { validate.validateRequiredFields(testObjectOne) }).not.to.throw('fieldOne is a required field but its value is empty.');
+        });
+    });
+
+    describe('Lengths', function(){
+        it('field values must not exceed their width (length) property', function(){
+            var testObjectOne = {
+                fieldOne: {
+                    name: 'fieldOne',
+                    width: 6,
+                    position: 9,
+                    required: false,
+                    type: 'alphanumeric',
+                    value: '1234567'
+                }
+            };
+
+            // The function should throw an error since the field's value exceeds its width.
+            expect(function () { validate.validateLengths(testObjectOne) }).to.throw('fieldOne\'s length is 7, but it should be no greater than 6.');
+
+            // Change the field's value to a string within the field's maximum width.
+            testObjectOne.fieldOne.value = 'isokay';
+
+            // Now that the value is within the field's limit, it shouldn't throw an error.
+            expect(function () { validate.validateLengths(testObjectOne) }).not.to.throw('fieldOne\'s length is 7, but it should be no greater than 6.');
+        });
+    });
+
+    describe('Data Types (alpha)', function(){
+        it('field values must correspond to the alpha data type', function(){
+                var testObjectOne = {
+                    fieldOne: {
+                        name: 'fieldOne',
+                        width: 10,
+                        position: 9,
+                        required: false,
+                        type: 'alpha',
+                        value: '#199#'
+                    }
+                };
+
+                // The function should throw an error since the field's data type doesn't match its contents.
+                expect(function () { validate.validateDataTypes(testObjectOne) }).to.throw('fieldOne\'s data type is required to be alpha, but its contents don\'t reflect that.');
+
+                // Change the field's value to a string within the field's maximum width.
+                testObjectOne.fieldOne.value = 'validvalue';
+
+                // Now that the value is within the field's limit, it shouldn't throw an error.
+                expect(function () { validate.validateDataTypes(testObjectOne) }).not.to.throw('fieldOne\'s data type is required to be alpha, but its contents don\'t reflect that.');
+        });
+    });
+
+    describe('Data Types (alphanumeric)', function(){
+        it('field values must correspond to the alphanumeric data type', function(){
+                var testObjectOne = {
+                    fieldOne: {
+                        name: 'fieldOne',
+                        width: 20,
+                        position: 9,
+                        required: false,
+                        type: 'alphanumeric',
+                        value: 'badData©'
+                    }
+                };
+
+                // The function should throw an error since the field's data type doesn't match its contents.
+                expect(function () { validate.validateDataTypes(testObjectOne) }).to.throw('fieldOne\'s data type is required to be alphanumeric, but its contents don\'t reflect that.');
+
+                // Change the field's value to a string within the field's maximum width.
+                testObjectOne.fieldOne.value = '@val1dval!';
+
+                // Now that the value is within the field's limit, it shouldn't throw an error.
+                expect(function () { validate.validateDataTypes(testObjectOne) }).not.to.throw('fieldOne\'s data type is required to be alphanumeric, but its contents don\'t reflect that.');
         });
     });
 });
