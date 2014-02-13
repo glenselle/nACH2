c592e5f517a6ebf3903f9f9e3d54d8994a1b49b4
Glen Selle <glenselle@me.com>
2014-02-13 11:45:46 -0600

    Cleaned up old comments and console.log statements and added the new logic for creating batches, adding entries to batches and generating the output string.

--- test.js
+++ test.js
@@ -1,22 +1,31 @@
-var Entry = require('./lib/entry');
+var Entry = require('./lib/entry')
+  , Batch = require('./lib/batch')
+  , File  = require('./lib/file');
 
-var test = new Entry({
+// var debitFile = new File({
+// 	type: 'debit'
+// });
+
+var creditLow = new Batch({
+	serviceClassCode: '220', // maybe we should allow something along the lines of credit=true as a higher-level api	
+	companyName: 'Zipline',
+	companyIdentification: '1234567890',
+	companyEntryDescription: 'Zip Transfer',
+	companyDescriptiveDate: 'Jan 3',
+	effectiveEntryDate: new Date(),
+	originatingDFI: '281074114'
+});
+
+var entry = new Entry({
 	receivingDFI: '081000210',
 	DFIAccount: '12345678901234567',
 	amount: '3521',
-	idNumber: 'RA8872D4H12jf31',
-	individualName: 'Glen!"#$%&()*+,-./:;<>=?@[]\ ^_`{}|~Selle',
-	discretionaryData: 'A1',
-	addendaId: '1'
+	idNumber: 'RAj##23920rjf31',
+	individualName: 'Glen Selle',
+	discretionaryData: 'A1'
 });
 
-console.log(test);
+creditLow.addEntry(entry);
+creditLow.addEntry(entry);
 
-// transactionCode
-// receivingDFI
-// DFIAccount
-// amount
-// idNumber
-// individualName
-// discretionaryData
-// addendaId
+console.log(creditLow.generateBatch());
