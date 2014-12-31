c4e136911b433232bcb36b6f653b2550ae1b6aad
Thomas <thomas@ubuntu.ubuntu-domain>
2014-12-31 16:07:44 -0600

    Rearrange output for NACHA file

--- test.js
+++ test.js
@@ -11,29 +11,34 @@ var companyIdentification = "983597258";
 var companyName = "Zipline Labs Inc";
 var transactionDiscription = 'Zip Transfer';
 	
-var debitFile = new File({
- 	type: 'debit',
+var achFile = new File({
 	immediateDestination: '281074114',
 	immediateOrigin: '123456789',
 	immediateDestinationName: 'Pulaski Bank',
 	immediateOriginName: 'Zipline Labs Inc.',
 	referenceCode: '#A000001',
-	header: {
-		recordTypeCode: {
-			value: '1'
-		}
-	}
 });
 
-var entry = new Entry({
+var creditTransaction = new Entry({
 	receivingDFI: '081000210',
 	DFIAccount: '12345678901234567',
-	amount: '3521',
+	amount: '35.21',
 	idNumber: 'RAj##23920rjf31',
 	individualName: 'Glen Selle',
 	discretionaryData: 'A1',
 	transactionCode:'22'
 });
+
+var debitTransaction = new Entry({
+	receivingDFI: '101000019',
+	DFIAccount: '923698412584',
+	amount: '35.21',
+	idNumber: 'RAj##23920rjf31',
+	individualName: 'Tom Horner',
+	discretionaryData: 'A1',
+	transactionCode:'27'
+});
+
 
 var creditLow = new Batch({
 	serviceClassCode: '220',
@@ -46,7 +51,7 @@ var creditLow = new Batch({
 	originatingDFI: routingNumber 
 });
 
-creditLow.addEntry(entry);
+creditLow.addEntry(creditTransaction);
 
 var creditHigh = new Batch({
 	serviceClassCode: '220',
@@ -59,17 +64,28 @@ var creditHigh = new Batch({
 	originatingDFI: routingNumber 
 });
 
-creditHigh.addEntry(entry);
-creditHigh.addEntry(entry);
+var debit = new Batch({
+	serviceClassCode: '225',
+	companyName: companyName,
+	standardEntryClassCode: 'PPD',
+	companyIdentification: companyIdentification,
+	companyEntryDescription: transactionDiscription,
+	companyDescriptiveDate: utils.computeBusinessDay(0).format('MMM D'),
+	effectiveEntryDate: new Date(utils.computeBusinessDay(0).format()),
+	originatingDFI: routingNumber 
+});
 
-debitFile.addBatch(creditLow);
-debitFile.addBatch(creditHigh);
+creditLow.addEntry(creditTransaction);
+creditHigh.addEntry(creditTransaction);
+debit.addEntry(debitTransaction);
 
-debitFile.generateFile(function(fileString){
+achFile.addBatch(creditLow);
+achFile.addBatch(creditHigh);
+achFile.addBatch(debit);
+
+achFile.generateFile(function(fileString){
 	fs.writeFile('NACHA.txt', fileString, function(err) {
 		if(err) console.log(err);
-
-		console.log('saved');
+		console.log(fileString);
 	})
-	console.log(fileString);
 });
